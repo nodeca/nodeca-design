70fac4a0bf15eda45c40377f3cfb90a4014a07dd
Vitaly Puzrin <vitaly@rcdesign.ru>
2011-07-01 22:39:24 +0400

    Partially updated forum categories layout + fixed style names

--- html/js/script.js
+++ html/js/script.js
@@ -1,10 +1,14 @@
 $(window).load(function(){
+
   $('.tgl-title').live('click',function(){
     $(this).toggleClass('title-drop-expanded').parent().next('.tgl-box').toggleClass('tgl-hide');
   });
+
+  // login menu menu
   $('.auth-toggle').click(function(){
     $(this).parent().toggleClass('auth-visible');
   });
+
   $('.frm-tgl-content,.tgl-box').each(function(){
     if ( $(this).hasClass('tgl-hide') ){
       $(this).removeClass('tgl-hide');
@@ -14,7 +18,23 @@ $(window).load(function(){
       $(this).height($(this).height());
     }
   });
+
   $('.frm-section-toggle').click(function(){
     $(this).toggleClass('frm-section-toggle-clicked').parent().parent().toggleClass('frm-tbl-section-hidden');
   });
+/*
+  $('.collapsable').each(function(){
+    if ( $(this).hasClass('collapsed') ){
+      $(this).removeClass('collapsed');
+      $(this).height($(this).height());
+      $(this).addClass('collapsed');
+    } else {
+      $(this).height($(this).height());
+    }
+  }
+*/
+  $('.collapser').click(function(){
+    $(this).parent().parent().toggleClass('collapsed');
+  });
+
 });
