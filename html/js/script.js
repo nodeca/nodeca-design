f7d4c5ec032891cc73b09924311d364ffa932180
Vitaly Puzrin <vitaly@rcdesign.ru>
2011-07-02 22:48:23 +0400

    updated profile togglers style

--- html/js/script.js
+++ html/js/script.js
@@ -10,8 +10,17 @@ $(window).load(function(){
   });
 
   // collapsable items animation
-  $('.collapser').click(function(){
-    $(this).parent().next().slideToggle('slow').parent().toggleClass('collapsed');
+//  $('.collapser').click(function(){
+//    $(this).parent().next().slideToggle('slow').parent().toggleClass('collapsed');
+  $('.collapser').each(function (i, el) {
+    var $this = $(el),
+        $togglers = $this,
+        $slave = $this.data('toggle') ? $($this.data('toggle')) : $this.parent().next();
+
+    $togglers.click(function (evt) {
+      $this.parent().parent().toggleClass('collapsed');
+      $slave.slideToggle('slow');
+    });
   });
 
 });
