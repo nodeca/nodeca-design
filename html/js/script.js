cb81c7ac6306a48868c8c16e16477fda06f1c633
Vitaly Puzrin <vitaly@rcdesign.ru>
2011-07-02 22:53:14 +0400

    removed outdated styles

--- html/js/script.js
+++ html/js/script.js
@@ -1,9 +1,5 @@
 $(window).load(function(){
 
-  $('.tgl-title').live('click',function(){
-    $(this).toggleClass('title-drop-expanded').parent().next('.tgl-box').toggleClass('tgl-hide');
-  });
-
   // login menu menu
   $('.auth-toggle').click(function(){
     $(this).parent().toggleClass('auth-visible');
