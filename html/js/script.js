8aa39128dd3d13d413b9f4c362ab2218d3cb4f3a
Vitaly Puzrin <vitaly@rcdesign.ru>
2011-07-04 23:19:07 +0400

    Added page resize button for debug

--- html/js/script.js
+++ html/js/script.js
@@ -5,7 +5,25 @@ $(window).load(function(){
     $(this).parent().toggleClass('auth-visible');
   });
 
-
   // init togglers for collapsable elements
   $('.collapser').ndCollapser();
+
+  // Debug code ===================
+  // Add button to switch page width
+  var dbg_width_arr = ['', '800px', '80%'], // '' is "default" (as in CSS)
+      dbg_width_idx = 0, // current width index
+      dbg_width_max = dbg_width_arr.length - 1; // upper bound
+  $('<a class="button">resize</a>').css({
+    position: 'absolute',
+    top: '5px',
+    left: '5px',
+    opacity: .3,
+    fontSize: '12px',
+    padding: '3px 5px'
+    
+  }).click(function () {
+    if (dbg_width_max < ++dbg_width_idx) { dbg_width_idx = 0; }
+    $('.trunk').css('width', dbg_width_arr[dbg_width_idx]);
+  }).appendTo(document.body);
+  // Debug code end ================
 });
